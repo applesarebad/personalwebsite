d2242c2fe1822d1e1419109ee036e2610fd0d886
Andrew Zhang <andrewzhang927@gmail.com>
2026-03-29 02:59:27 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://applesarebad.github.io',
+    base: '/andrewz',
     devToolbar: {
         enabled: false
-      }
+    }
 });
