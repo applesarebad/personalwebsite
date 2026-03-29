4ae074c2c46c266609d617060935c19bfb424d3e
applesarebad <andrewzhang927@gmail.com>
2026-03-29 13:13:51 -0400

    new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://applesarebad.github.io',
-    base: '/personalwebsite',
+    site: 'https://andrew-zhang.ca'
 });
 
